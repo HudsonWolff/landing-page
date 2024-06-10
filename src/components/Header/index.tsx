39001af1d90250ae37f12dc74e3052c0c34a68fb
HudsonWolff <hudson.guilherme@hotmail.com>
2024-06-10 19:31:22 -0300

    feat add hover and button-default and component tickets

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -7,11 +7,13 @@ function Header() {
 <img src="src\assets\logo.jpg" alt="logo do evento AnimeConnect" />
 <nav>
 <ul>
+<li> <a href="#section-hero">Home</a>  </li>
     <li> <a href="">Sobre</a>  </li>
     <li> <a href="">Cronograna</a>  </li>
-    <li><a href=""> Ingressos</a> </li>
+    <li><a href="#section-tickets"> Ingressos</a> </li>
     <li> <a href="">Localização</a> </li>
     <li> <a href="">Contato</a> </li>
+    <li> <a href="#section-form">NewsLetter</a> </li>
 </ul>
 
 
