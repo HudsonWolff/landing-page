15c69a25d20877c33143bc9a141867ce58437c41
HudsonWolff <hudson.guilherme@hotmail.com>
2024-06-12 19:24:11 -0300

    feat add Header function

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,19 +1,45 @@
-
+import { useEffect, useState } from "react";
 import './style.css'
 
 function Header() {
+  const [scrolled, setScrolled] = useState(false);
+  const [hidden, setHidden] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrolled(window.scrollY > 50);
+    };
+
+    const handleMouseMove = () => {
+      setHidden(false);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    window.addEventListener("mousemove", handleMouseMove);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+      window.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, []);
+
+  const handleLinkClick = () => {
+    setHidden(true);
+  };
+  
   return (
-    <header>
+   
+      <header className={`${scrolled ? 'scrolled' : ''} ${hidden ? 'hidden' : ''}`}>
 <img src="src\assets\logo.jpg" alt="logo do evento AnimeConnect" />
 <nav>
 <ul>
-<li> <a href="#section-hero">Home</a>  </li>
-    <li> <a href="#about-section">Sobre</a>  </li>
-    <li> <a href="#schedule-section">Cronograna</a>  </li>
-    <li><a href="#section-tickets"> Ingressos</a> </li>
-    <li> <a href="#location-section">Localização</a> </li>
-    <li> <a href="#contact-section">Contato</a> </li>
-    <li> <a href="#section-form">NewsLetter</a> </li>
+<li> <a href="#section-hero" onClick={handleLinkClick}>Home</a>  </li>
+    <li> <a href="#about-section" onClick={handleLinkClick}>Sobre</a>  </li>
+    <li> <a href="#schedule-section" onClick={handleLinkClick}>Cronograna</a>  </li>
+    <li><a href="#section-tickets" onClick={handleLinkClick}> Ingressos</a> </li>
+    <li> <a href="#location-section" onClick={handleLinkClick}>Localização</a> </li>
+    <li> <a href="#contact-section" onClick={handleLinkClick}>Contato</a> </li>
+    <li> <a href="#section-form"  onClick={handleLinkClick}>NewsLetter</a> </li>
 </ul>
 
 
@@ -21,7 +47,7 @@ function Header() {
 </nav>
 
 
-    </header>
+</header>
   )
 }
 
